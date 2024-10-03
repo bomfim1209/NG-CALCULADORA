4e468efbd332392587aa956755a6e3bb4d98dae6
Matheus Bomfim <matheus.bomfim@uol.com.br>
2024-10-03 18:59:53 -0300

    primeiro commit

---
+++ src/app/teclado/teclado.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-teclado',
+  templateUrl: './teclado.component.html',
+  styleUrl: './teclado.component.css'
+})
+export class TecladoComponent {
+  public resultado: string = "";
+
+  clicar(valor: string) {
+    this.resultado += valor;
+  }
+  
+  finalizar() {
+    this.resultado = eval(this.resultado).toString();
+  }
+
+}
